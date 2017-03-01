2e296c6c9826e01167f5c35e949e5e432507cbc0
Sascha Scherrer <ddragbone@gmail.com>
2017-03-01 20:38:48 +0100

    Fixed run script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ curl -m 5 localhost:1024/DEATHPILL | true
 sleep 5
 rm build/deploy/*.jar
 cp build/libs/pizzaslackbot-*.jar build/deploy/pizzaslackbot.jar
-BUILD_ID=dontKillMe nohup bash java -jar build/deploy/pizzaslackbot.jar $1 $2 > log.txt 2>&1
+BUILD_ID=dontKillMe nohup java -jar build/deploy/pizzaslackbot.jar $1 $2 > log.txt 2>&1 &
