f83fad0f28286ca225deb368317efa31915b40bc
Sascha Scherrer <ddragbone@gmail.com>
2017-03-01 19:25:49 +0100

    Moved deploy code to run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-java -jar build/deploy/pizzaslackbot-alpha.jar $1 $2 > log.txt 2>&1
+curl localhost:1024/DEATHPILL
+sleep 5
+rm build/deploy/*.jar
+cp build/libs/pizzaslackbot-*.jar build/deploy/pizzaslackbot.jar
+BUILD_ID=dontKillMe nohup bash java -jar build/deploy/pizzaslackbot.jar $1 $2 > log.txt 2>&1
